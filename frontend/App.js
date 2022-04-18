c7f6846ea9baf7d1d29f4347c3ecc5eec0b4ccee
Hampus Andersson <70724079+Hampster8@users.noreply.github.com>
2022-04-18 14:01:28 +0200

    Fixed Sorting
    Eureka

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Table from './src/components/Table';
+import './src/index.css'
 
 const App = () => {
   return (
-    <div className="table_container">
-      <Table />
+
+    <div className="container">
+      <h1 align="center">Recent Registrations</h1>
+      <div className="table table-bordered">
+        <Table />
+      </div>
     </div>
+
   );
 };
 
--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -12,6 +12,25 @@ const Table = () => {
   useEffect(() =>{
     getDb()
   },[])
+  
+  const [order, setOrder] = useState('ASC');
+    const sorting = (col) => {
+      if (order === "ASC"){
+        const sorted = [...data].sort((a,b)=>
+          String(a[col]).toLowerCase() > String(b[col]).toLowerCase() ? 1 : -1
+        );
+        setData(sorted);
+        setOrder("DSC")
+      }
+      if (order === "DSC"){
+        const sorted = [...data].sort((a,b)=>
+        String(a[col]).toLowerCase() < String(b[col]).toLowerCase() ? 1 : -1
+        );
+        setData(sorted);
+        setOrder("ASC")
+      }
+    }
+
 const tableData = data.map((item,key) =>{
   return (
     <tr key={key}>
@@ -22,20 +41,21 @@ const tableData = data.map((item,key) =>{
     </tr>
   )
 })
+//code after <tr></tr>
   return (
     <div className="table_container">
       <table>
-      <thead>
-        <tr>
-          <th>Student ID</th>
-          <th>Full Name</th>
-          <th>Course Name</th>
-          <th>Registration Time</th>
-        </tr>
-      </thead>
-        <tbody>
-          {tableData}
-        </tbody>
+        <thead>
+          <tr> 
+            <th onClick={() => sorting("student._id")}>Student ID</th>
+            <th onClick={() => sorting("student.full_name")}>Full Name</th>
+            <th onClick={() => sorting("course.course_name")}>Course Name</th>
+            <th onClick={() => sorting("unix_timestamp")}>Registration Time</th>
+          </tr>
+        </thead>
+          <tbody>
+            {tableData}
+          </tbody>
       </table>
     </div>
   );
